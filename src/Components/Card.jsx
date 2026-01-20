101793c5174cbaa8c36c70aa36696f37a824983b
YasmeenMuhammed <yasmeenmuhammed69@gmail.com>
2026-01-21 01:57:40 +0300

    Fix CategoriesList import case

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -6,9 +6,9 @@ import { Link } from 'react-router-dom';
 export default function Card({ meal }) {
     const { idMeal, strMeal, strArea, strMealThumb: mealImg } = meal;
     return <>
-        <div className="card hover:shadow-xl sm:col-span-12 md:col-span-6 lg:col-span-3 bg-white items-center  rounded-4xl p-4 group hover:scale-105 transition-all duration-300 mt-12">
+        <div className="meal-card hover:shadow-xl sm:col-span-12 md:col-span-6 lg:col-span-3 bg-white items-center  rounded-4xl p-4 group hover:scale-105 transition-all duration-300 mt-12">
             <div className="-translate-y-20">
-            <img src={mealImg} className=' size-45 rounded-full m-auto shadow-2xl drop-shadow-2xl group-hover:rotate-[360deg] duration-700 transition-transform' alt="" />
+            <img src={mealImg} className='meal-img size-45 rounded-full m-auto shadow-2xl drop-shadow-2xl duration-700 transition-transform' alt="" />
             </div>
 
             <div className="card-content flex flex-col items-center gap-4 -translate-y-5">
